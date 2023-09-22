4f5f817178a28d49595162438a9b800aefdc6353
Eliseu Gomes da Silva <eliseu.silva@smartconsulting.com.br>
2023-09-22 12:25:45 -0300

    Plugin: Criar Tarefa

---
+++ repositorio-dev-dynamics/repositorio-dev-dynamics/Plugins/criarTarefa.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repositorio_dev_dynamics.Plugins
+{
+    public class criarTarefa : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service.
+            // Obtendo o rastreamento do serviço.
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            // Obtendo o contexto de execução do provedor do serviço.
+
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            // A Coleção dos Parâmetros de Entrada contém todos os dados passados na menssagem da solicitação.
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                // Obtem a entidade alvo dos parâmetros de entrada.
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Obtain the IOrganizationService instance which you will need for
+                // Obtem a instancia IOrganizationService que você irá precisar para 
+                // web service calls.
+                // Solicitações de serviços Web.
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    // Plug-in business logic goes here.  
+
+                    // Create a task activity to follow up with the account customer in 7 days. 
+                    // Criar uma atividade de tarefa de "Follow up" para a conta do cliente em 7 dias.
+
+                    Entity followup = new Entity("task"); // definir a entidade do registro.
+
+                    // Set valores paras os campos do registro.
+
+                    followup["subject"] = "Enviar e-mail para o novo Cliente.";
+                    followup["description"] =
+                        "Follow up with the customer. Check if there are any new issues that need resolution.";
+                    followup["scheduledstart"] = DateTime.Now.AddDays(7);
+                    followup["scheduledend"] = DateTime.Now.AddDays(7);
+                    followup["category"] = context.PrimaryEntityName;
+
+                    // Refer to the account in the task activity.
+                    // Referir-se a conta da atividade da tarefa.
+                    if (context.OutputParameters.Contains("id"))
+                    {
+                        Guid regardingobjectid = new Guid(context.OutputParameters["id"].ToString());
+                        string regardingobjectidType = "account";
+
+                        followup["regardingobjectid"] =
+                        new EntityReference(regardingobjectidType, regardingobjectid);
+                    }
+
+                    // Create the task in Microsoft Dynamics CRM.
+                    // Criar a tarefa no Microsoft Dynamics CRM.
+                    tracingService.Trace("FollowupPlugin: Creating the task activity.");
+                    service.Create(followup);
+
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+
+    }
+}
